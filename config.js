2b453f24b9dd8500dff029e8a153c959dd5bcd15
Hannan Ali <abdulhannanali@outlook.com>
2016-03-28 14:32:51 +0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-var env = process.env
-
-
-env["MONGODB_CONNECTION_URI"] = "mongodb://programmer:location3@ds028559.mlab.com:28559/locationbot"
-env["BOT_TOKEN"] = "181614480:AAE9k6LixAMDecBwymnkrF75Ms5bitYH9t8"
